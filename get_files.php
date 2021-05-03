adcfafb2e83adab2f4f157134064717cd2903d45
aOelschlager <41480572+aOelschlager@users.noreply.github.com>
2021-05-03 11:14:43 -0500

    Update get_files.php

--- get_files.php
+++ get_files.php
@@ -9,14 +9,14 @@ $csv = array_map('str_getcsv', file('file_urls4.csv'));
 foreach ($csv as $row) {
 	$pid = $row[0]; //unique id of object
 	$file_name = $row[1]; //title of object
-    $url = $row[2]; //url for download
+    	$url = $row[2]; //url for download
 
-    if(file_put_contents($file_name, file_get_contents($url))) {
-        echo "$file_name downloaded successfully";
-    }
-    else {
-        echo "$file_name downloading failed.";
-    }
+    	if(file_put_contents($file_name, file_get_contents($url))) {
+        	echo "$file_name downloaded successfully";
+    	}
+    	else {
+        	echo "$file_name downloading failed.";
+    	}
 }
 
 ?>
