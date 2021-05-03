05a4a93a546734e05589c5a4dac00fc5eb0d91af
aOelschlager <41480572+aOelschlager@users.noreply.github.com>
2021-05-03 10:45:56 -0500

    Update convert_files.php

--- convert_files.php
+++ convert_files.php
@@ -1,6 +1,6 @@
 /*
 	Converts downloaded jp2 files into tifs using imageMagick.
-    These were files that were not diplaying on the website.
+    	These were files that were not diplaying on the website.
    	4/21/2021
 */
 
@@ -10,15 +10,14 @@ $csv = array_map('str_getcsv', file('file_urls4.csv'));
 foreach ($csv as $row) {
 	$pid = $row[0];
 	$file_name = $row[1];
-    $url = $row[2];
+    	$url = $row[2];
 
-    $new_file_name = $file_name . ".tiff";
+    	$new_file_name = $file_name . ".tiff";
 
-    $jp2_img = $file_name . " " . $new_file_name;
+    	$jp2_img = $file_name . " " . $new_file_name;
 
-    print "Converting: " . $file_name . "\n";
-    exec("convert $jp2_img");
-
+    	print "Converting: " . $file_name . "\n";
+    	exec("convert $jp2_img");
 }
 
 ?>
