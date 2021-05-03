dfca89c3017c57c8dde3dc9e1b4115c437bea0a8
aOelschlager <41480572+aOelschlager@users.noreply.github.com>
2021-05-03 11:18:09 -0500

    Update move_sb_folders.php

--- move_sb_folders.php
+++ move_sb_folders.php
@@ -4,22 +4,22 @@
 */
 
 <?php
-    $dir = "D:/single_books_ex_hd_2";
-    $cdir = scandir($dir);
-    foreach ($cdir as $key => $value) {
+$dir = "D:/single_books_ex_hd_2";
+$cdir = scandir($dir);
+foreach ($cdir as $key => $value) {
         if (!in_array($value,array(".",".."))) {
-            if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
-                $dir2 = $dir . DIRECTORY_SEPARATOR . $value;
-                $cdir2 = scandir($dir2);
-                foreach ($cdir2 as $key2 => $value2) {
-                    if (!in_array($value2,array(".",".."))) {
-                        if (is_dir($dir2 . DIRECTORY_SEPARATOR . $value2)) {
-                            $dir3 = $dir2 . DIRECTORY_SEPARATOR . $value2;
-                            print $dir3 . "\n";
-                        }
-                    }
-                }
-            }
+            	if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+                	$dir2 = $dir . DIRECTORY_SEPARATOR . $value;
+                	$cdir2 = scandir($dir2);
+                	foreach ($cdir2 as $key2 => $value2) {
+                    		if (!in_array($value2,array(".",".."))) {
+                        		if (is_dir($dir2 . DIRECTORY_SEPARATOR . $value2)) {
+                            		$dir3 = $dir2 . DIRECTORY_SEPARATOR . $value2;
+                            		print $dir3 . "\n";
+                        		}
+                    		}
+                	}
+            	}
         }
-    }               
+}               
 ?>
